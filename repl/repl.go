75a116a4e797f9eb6f1032eefcf5eb0b16c75ef7
zzg <1459181201@qq.com>
2025-11-09 21:00:47 +0800

    实现表示对象

--- repl/repl.go
+++ repl/repl.go
@@ -2,29 +2,64 @@
 package repl
 
 import (
-    "bufio"
-    "fmt"
-    "go-monkey-compiler/token"
-    "go-monkey-compiler/lexer"
-    "io"
+	"bufio"
+	"fmt"
+	"go-monkey-compiler/lexer"
+	"go-monkey-compiler/parser"
+	"io"
 )
 
 const PROMPT = ">> "
 
+const WELCOME = `Welcome to the Monkey Programming Language!
+
+`
+
+const MONKEY_FACE = `
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._ _./  |
+       \   \.-./   /
+        '._/___\_.'
+         __\___/__
+        '"""""""""'
+`
+
 func Start(in io.Reader, out io.Writer) {
-    scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
+	fmt.Fprint(out, WELCOME)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
 
-    for {
-        scanned := scanner.Scan()
-        if !scanned {
-            return
-        }
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-        line := scanner.Text()
-        l := lexer.New(line)
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
 
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Fprintf(out, "%+v\n", tok)
-        }
-    }
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
