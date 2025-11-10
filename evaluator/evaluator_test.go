df15f767c7b3fb9e8d5d4372e657fc3622c0c0d8
zzg <1459181201@qq.com>
2025-11-10 20:35:18 +0800

    实现前缀表达式

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -14,6 +14,8 @@ func TestEvalInteferExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -48,6 +50,12 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
