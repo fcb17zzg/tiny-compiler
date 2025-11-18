6de81010ccec48f6601e69f24f3dff3438151e6e
zzg <1459181201@qq.com>
2025-11-18 21:29:22 +0800

    实现最小虚拟机(取指->解码->执行)

---
+++ vm/vm.go
@@ -0,0 +1,60 @@
+package vm
+
+import (
+	"fmt"
+	"go-monkey-compiler/code"
+	"go-monkey-compiler/compiler"
+	"go-monkey-compiler/object"
+)
+
+const StackSize = 2048
+
+type VM struct {
+	constant     []object.Object
+	instructions code.Instructions
+	stack        []object.Object
+	sp           int // 始终指向栈顶的下一个空位置。栈顶的值是 stack[sp-1]
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		constant:     bytecode.Constants,
+		instructions: bytecode.Instructions,
+		stack:        make([]object.Object, StackSize),
+		sp:           0,
+	}
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			err := vm.push(vm.constant[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
---
+++ vm/vm_test.go
@@ -0,0 +1,79 @@
+package vm
+
+import (
+	"fmt"
+	"go-monkey-compiler/ast"
+	"go-monkey-compiler/compiler"
+	"go-monkey-compiler/lexer"
+	"go-monkey-compiler/object"
+	"go-monkey-compiler/parser"
+	"testing"
+)
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testIntegerObject(expected int64, actual interface{}) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
+	return nil
+}
+
+type runVmTestCase struct {
+	input    string
+	expected int64
+}
+
+func runVmTests(t *testing.T, tests []runVmTestCase) {
+	t.Helper()
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(comp.Bytecode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.StackTop()
+
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []runVmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2},
+	}
+
+	runVmTests(t, tests)
+}
