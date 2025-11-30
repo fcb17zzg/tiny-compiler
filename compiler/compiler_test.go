3e3b67591a8baff7e53ce7e97be9917e8ac2130a
zzg <1459181201@qq.com>
2025-11-30 17:36:24 +0800

    编译函数调用

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -594,3 +594,44 @@ func TestFunctionWithoutReturnValue(t *testing.T) {
 	}
 	runCompilerTests(t, tests)
 }
+
+func TestFunctionCalls(t *testing.T) {
+	tests := []CompilerTestCase{
+		{
+			input: `fn() { 24 }();`,
+			expectedConstants: []interface{}{
+				24,
+				[]code.Instructions{
+					code.Make(code.OpConstant, 0),
+					code.Make(code.OpReturnValue),
+				},
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpCall),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: `
+			let noArg = fn() { 24 };
+			noArg();
+			`,
+			expectedConstants: []interface{}{
+				24,
+				[]code.Instructions{
+					code.Make(code.OpConstant, 0),
+					code.Make(code.OpReturnValue),
+				},
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpGetGlobal, 0),
+				code.Make(code.OpCall),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+	runCompilerTests(t, tests)
+}
